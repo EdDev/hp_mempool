bf891a95bc128b49e0b6e2c71df70cf6a72dbf3b
Edward Haas <edwardh.dev@gmail.com>
2015-04-21 00:58:34 +0300

    Integrated the high performance ring queue.
    The ring queue is based on the DPDK project,
    which in turn is based on a FreeBSD implementation.

--- src/objmempool.h
+++ src/objmempool.h
@@ -12,12 +12,15 @@
 #define UNUSED(x) (void(x))
 #endif
 
+#include "rte/rte_ring.h"
 #include <list>
+
+#define POWEROF2(x) ((((x)-1) & (x)) == 0)
 
 template <typename OBJ_TYPE>
 class Objmempool
 {
-    typedef std::list<OBJ_TYPE *> Free_list;
+    typedef rte_ring Free_list;
 
 protected:
     Objmempool() {};
@@ -27,38 +30,52 @@ public:
     static void * operator new (size_t size)
     {
         UNUSED(size);
-        void * obj = free_list->front();
-        free_list->pop_front();
+        void * obj_array[1];
+        const unsigned int num = 1;
+        unsigned int n = rte_ring_dequeue_burst(free_list, obj_array, num);
+        UNUSED(n);
+
+        void * obj = obj_array[0];
         return obj;
     }
 
     static void operator delete (void * m)
     {
-        free_list->push_front(static_cast<OBJ_TYPE *>(m));
+    	const unsigned int num = 1;
+    	unsigned int n = rte_ring_enqueue_burst(free_list, &m, num);
+    	UNUSED(n);
     }
 
     static void mempool_create(size_t object_count)
     {
         if(NULL == free_list)
         {
-            free_list = ::new Free_list();
-            obj_count = object_count;
+            //free_list = ::new Free_list();
+        	free_list = new_free_list("noname", object_count, 0);
+            obj_count = object_count-1;
 
-            for (int i=0; i < object_count; ++i)
-                free_list->push_back(::new OBJ_TYPE());
+            for (size_t i=0; i < obj_count; ++i)
+            {
+                OBJ_TYPE * const obj = ::new OBJ_TYPE();
+                void * const vobj = static_cast<void*>(obj);
+                rte_ring_enqueue_burst(free_list, &vobj, 1);
+            }
         }
     }
 
     static void mempool_destroy()
     {
-        for (int i=0; i < obj_count; ++i)
+        for (size_t i=0; i < obj_count; ++i)
         {
-            OBJ_TYPE * obj = free_list->front();
+            void * obj_array[1];
+            const unsigned int num = 1;
+            rte_ring_dequeue_burst(free_list, obj_array, num);
+
+            OBJ_TYPE * obj = static_cast<OBJ_TYPE*>(obj_array[0]);
             ::delete obj;
-            free_list->pop_front();
         }
 
-        ::delete free_list;
+        free(free_list);
         obj_count = 0;
         free_list = NULL;
     }
@@ -66,12 +83,37 @@ public:
     static size_t get_mempool_size()
     {
         if(NULL != free_list)
-            return free_list->size();
+            return rte_ring_count(free_list);
         else
             return 0;
     }
 
 private:
+
+    static void round_up_to_a_powerof2(uint32_t & size)
+    {
+        // Assumes 32 bit size.
+        --size;
+        size |= size >> 1;
+        size |= size >> 2;
+        size |= size >> 4;
+        size |= size >> 8;
+        size |= size >> 16;
+        ++size;
+    }
+
+    static rte_ring * new_free_list(std::string _name, unsigned int q_size, unsigned int type)
+	{
+        if(! POWEROF2(q_size))
+            round_up_to_a_powerof2(q_size);
+
+        rte_ring * ring = rte_ring_create(_name.c_str(), q_size, SOCKET_ID_ANY, type);
+        if(NULL == ring)
+            abort();
+
+        return ring;
+	}
+
     static Free_list * free_list;
     static size_t obj_count;
 };
--- unit_tester/tests/test_objmempool.cpp
+++ unit_tester/tests/test_objmempool.cpp
@@ -44,7 +44,7 @@ TEST(mempool, create_and_destroy_pool__check_pool_size)
 {
     const size_t pool_size = 32;
     Test_object::mempool_create(pool_size);
-    LONGS_EQUAL(pool_size, Test_object::get_mempool_size());
+    LONGS_EQUAL(pool_size-1, Test_object::get_mempool_size());
     Test_object::mempool_destroy();
     LONGS_EQUAL(0, Test_object::get_mempool_size());
 }
@@ -56,10 +56,10 @@ TEST(mempool, new_delete_1_object__check_pool_size_decreased_by_1)
     Test_object::mempool_create(pool_size);
 
     Test_object * obj = new Test_object;
-    LONGS_EQUAL(pool_size - 1, Test_object::get_mempool_size());
+    LONGS_EQUAL(pool_size - 2, Test_object::get_mempool_size());
 
     delete obj;
-    LONGS_EQUAL(pool_size, Test_object::get_mempool_size());
+    LONGS_EQUAL(pool_size - 1, Test_object::get_mempool_size());
 
     Test_object::mempool_destroy();
 }
